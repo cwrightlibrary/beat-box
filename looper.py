2a98d9e8007898bac870f6b43e55f9b6f67472a3
cwrightlibrary <108359193+cwrightlibrary@users.noreply.github.com>
2024-03-19 13:00:42 -0400

    Add files via upload

--- looper.py
+++ looper.py
@@ -1,42 +1,89 @@
-import pygame
-from customtkinter import *
+from midi_mapping import *
+from note_lengths import *
+from os.path import dirname, join, realpath
+from scamp import *
+from time import sleep
 
-def play_wav(file1, file2):
-    pygame.mixer.init()
-    sound1 = pygame.mixer.Sound(file1)
-    sound2 = pygame.mixer.Sound(file2)
-    channel1 = sound1.play()
-    channel2 = sound2.play()
-    
-    while channel1.get_busy() or channel2.get_busy():
-        pygame.time.delay(100)
-    
-    pygame.mixer.quit()
+TEMPO = 90
+SESSION = Session(tempo=TEMPO)
+DRUMMER_SF = join(dirname(realpath(__file__)), "drum-kit.sf2")
+PIANO_SF = join(dirname(realpath(__file__)), "steinway.sf2")
 
-class App(CTk):
-    def __init__(self):
-        super().__init__()
-        self.title("Beat Maker")
-        self.geometry("400x400")
-        pygame.mixer.init()
-        
-        self.kick = "kick.wav"
-        self.hihat = "hihat.wav"
-        self.snare = "snare.wav"
-    
-    def play_audios(self, sounds: list):
-        kick = False
-        hihat = False
-        snare = False
-        for sound in sounds:
-            if "kick" in sound:
-                kick = True
-            if "hihat" in sound:
-                hihat = True
-            if "snare" in sound:
-                snare = True
+DRUMMER = SESSION.new_part("Brush", soundfont=DRUMMER_SF)
+PIANO = SESSION.new_part("Piano", soundfont=PIANO_SF)
 
-if __name__ == "__main__":
-    file1 = "kick.wav"
-    file2 = "snare.wav"
-    play_wav(file1, file2)
+BEATS = [1, 2, 3, 4]
+C_BEAT = 0
+
+BRUSH_SWIRLS = electric_snare
+HI_HAT_CLOSED = closed_hi_hat
+HI_HAT_OPEN = open_hi_hat
+HI_HAT_PEDAL = pedal_hi_hat
+KICK = bass_drum_1
+RIDE = ride_cymbal_1
+SNARE = tap_snare
+HI_HAT_PEDAL = pedal_hi_hat
+NO_INSTRUMENT = None
+
+
+def play_bg(drummer):
+    while True:
+        drummer.play_note(BRUSH_SWIRLS, 1, 4)
+
+
+def playnote(drum_note, length, volume, drummer):
+    if len(drum_note) == 1 or (len(drum_note) == 2 and None in drum_note):
+        if len(drum_note) == 1 and drum_note[0] == None:
+            drummer.play_note(electric_snare, 0, length, silent=True)
+        else:
+            drummer.play_note(drum_note[0], volume, length)
+    elif len(drum_note) > 1:
+        if None in drum_note:
+            temp_notes = []
+            for n in drum_note:
+                if n != None:
+                    temp_notes.append(n)
+            if len(temp_notes) == 0:
+                drummer.play_note(electric_snare, 0, length, silent=True)
+            elif len(temp_notes) > 1:
+                drummer.play_chord(temp_notes, volume, length)
+            else:
+                drummer.play_note(temp_notes[0], volume, length)
+        else:
+            drummer.play_chord(drum_note, volume, length)
+
+
+def jazz_pattern(note, drummer):
+    if note == 1:
+        playnote([KICK, RIDE], quarter_note, 1, drummer)
+    elif note == 2:
+        playnote([SNARE, RIDE], triplet_quarter_note * 2, 1, drummer)
+        playnote([KICK, RIDE], triplet_quarter_note, 1, drummer)
+    elif note == 3:
+        playnote([RIDE], triplet_quarter_note, 1, drummer)
+        playnote([SNARE], triplet_quarter_note * 2, 0.3, drummer)
+    elif note == 4:
+        playnote([SNARE, RIDE], triplet_quarter_note * 2, 1, drummer)
+        playnote([SNARE], triplet_quarter_note, 0.4, drummer)
+
+
+def chords(note, piano):
+    if note == 1:
+        piano.play_chord([60, 64, 67, 71], 0.8, quarter_note)
+    elif note == 2:
+        piano.play_chord([64, 67, 71, 74], 0.8, quarter_note)
+    elif note == 3:
+        piano.play_chord([60, 64, 67, 71], 0.8, quarter_note)
+    elif note == 4:
+        piano.play_chord([64, 67, 71, 74], 0.8, quarter_note)
+
+
+fork(play_bg, args=(DRUMMER,))
+sleep(1)
+while C_BEAT < len(BEATS):
+    print(str(BEATS[C_BEAT]))
+    fork(chords, args=(BEATS[C_BEAT], PIANO,))
+    jazz_pattern(BEATS[C_BEAT], DRUMMER)
+    C_BEAT += 1
+    if C_BEAT == len(BEATS):
+        C_BEAT = 0
