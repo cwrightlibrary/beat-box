aeb7ae7804995d28223408277e19937d5091d1be
cwrightlibrary <108359193+cwrightlibrary@users.noreply.github.com>
2024-03-21 16:56:32 -0400

    Add files via upload

--- looper-app/songs/lonnies-lament/image_gen.py
+++ looper-app/songs/lonnies-lament/image_gen.py
@@ -143,9 +143,6 @@ def music(info: list, notes: str, song_name: str, midi=False, swing=False):
             repeat_number = int(new_v[0])
             vol = int(new_v[1]) - 1
             measures[vol].append(spanner.RepeatBracket(measures[vol], number=repeat_number))
-
-    
-    measures[6].append(spanner.RepeatBracket(measures[6:9], number=1))
     
     part = stream.Part()
     for m in measures:
@@ -160,18 +157,16 @@ def music(info: list, notes: str, song_name: str, midi=False, swing=False):
         remove(join(current_dir, song_name + ".musicxml"))
         rename(join(current_dir, song_name + "-1.png"), join(current_dir, song_name + ".png"))
 
+# EXAMPLES
+# c5/1 is middle C quarter note length
+# !cmaj7 is a chord symbol for Cmaj7
+# { starts a tie, } ends a tie
+# ^ ends the current measure and starts a new line
+# [2s]9e[ puts a start repeat on the 2nd measure, end repeat on the 9th measure
+# ]i1i7-9@i2i10-12] puts a repeat bracket over measures 7-9 with a 1, and a bracket
+# over 10-12 with a 2
+# |12&20| puts double barlines on measures 12 and 20
+info = ["Treble", C, "4/4"]
+notes = "r/3 g4/.5 f4/.5 !cm7 e-4/1.5 d4/.25 c4/.25 !dm7 d4/.5 f4/1.5 !e-maj7"
 
-info = ["Treble", G, "4/4"]
-notes = "r/1 e4/1 f#4/1 g4/1 !am7 c5/4{ !d7 c5/1} d4/1 e4/1 f#4/1 !gmaj7 b4/2 b4/2{ ^"
-notes += " !cmaj7 b4/1} c4/1 d4/1 e4/1 !f#m7b5 a4/4{ !b7 a4/1} b3/1 c#4/1 d#4/1 !em g4/4 ^"
-notes += " r/1 e4/1 f#4/1 g4/1 !b7 a4/1 f#4/1 a4/1 g4/1 !em e4/4{ e4/1} r/1 d#4/1 e4/1 ^"
-
-notes += " !f#m7b5 f#4/1 b3/1 f#4/2{ !b7b9 f#4/1} f#4/1 e4/1 f#4/1 !em g4/4{ g4/1} g4/1 f#4/1 g4/1 ^"
-notes += " !am7 a4/4{ !d7 a4/1} d4/1 d5/1 c5/1 !gmaj7 b4/4{ b4/1} r/1 a#4/1 b4/1 ^"
-
-notes += " !f#m7b5 c5/1 c5/1 a4/1 a4/1 !b7b9 f#4/3 c5/1 !em7 b4/2 !e-7 b4/2{ !dm7 b4/2{ !d-7 b4/1} e4/1 ^"
-notes += " !cmaj7 a4/3 g4/1 !b7b9 f#4/2 g4/1 b3/1 !em e4/4 r/1 e4/1 f#4/1 g4/1"
-
-notes += " [2s]9e[ ]i1i7-9@i2i10-12] |12&20|"
-
-music(info, notes, "Autumn Leaves", False, False)
+music(info, notes, "Lonnie's Lament", False, False)
